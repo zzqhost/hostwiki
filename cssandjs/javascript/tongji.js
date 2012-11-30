5f50f78525a3427e5cc5b9c04cabba0b071db1d2
b649 <b649@borqs.com>
2012-11-30 21:02:37 +0800

    修改主页的统计js for test

--- cssandjs/javascript/tongji.js
+++
@@ -1,2 +0,0 @@
-var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F0cbe0839852542956fe17f62ec5746ce' type='text/javascript'%3E%3C/script%3E"));
