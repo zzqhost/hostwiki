21234a80e1ad9ea1341e9ff185296840958a25d2
b649 <b649@borqs.com>
2012-11-30 21:09:11 +0800

    把统计及disqus的脚本放到单独文件中

--- cssandjs/javascript/tongjianddisqus.js
+++ cssandjs/javascript/tongjianddisqus.js
@@ -1,6 +1,7 @@
         var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
         document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F0cbe0839852542956fe17f62ec5746ce' type='text/javascript'%3E%3C/script%3E"));
 
+
         var disqus_identifier = 'host_%title%'
 
         /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
